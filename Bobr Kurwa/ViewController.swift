1af115854819bab9aa3b978d4fc2f473143aca52
Константин Гончаров <goncharov.ka2312@gmail.com>
2024-05-31 05:28:07 +0300

    add: GIF image view

--- Bobr Kurwa/ViewController.swift
+++ Bobr Kurwa/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet var curTemp: UILabel!
     @IBOutlet var curSpeed: UILabel!
     @IBOutlet var curTime: UILabel!
+    @IBOutlet var curInterval: UILabel!
+    @IBOutlet var gif: UIImageView!
     
     override func loadView() {
         super.loadView()
@@ -22,6 +24,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
+        let wetherGif = UIImage.gifImageWithName("bert-kreischer-dance")
+        gif.image = wetherGif
 //        getData()
 //        button.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
     }
@@ -42,9 +46,10 @@ class ViewController: UIViewController {
           
             if let data, let wether = try? JSONDecoder().decode(Parser.self, from: data) {
                 DispatchQueue.main.async {
-                    self.curTemp.text = "Температура \(wether.current.temperature2M) °"
-                    self.curTime.text = "\(wether.current.time)"
-                    self.curSpeed.text = "Скорость ветра \(wether.current.windSpeed10M) м/с"
+                    self.curTemp.text = "Температура  \(wether.current.temperature2M) °С"
+                    self.curTime.text =  "\(String(wether.current.time).prefix(10))  \(String(wether.current.time).suffix(5))"
+                    self.curSpeed.text = "Скорость ветра  \(wether.current.windSpeed10M) м/с"
+                    self.curInterval.text = "Ощущается как  \(wether.current.apparentTemperature) °С"
                 }
             }
             
@@ -55,15 +60,15 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print("viewWillAppear")
+        getData()
+        Timer.scheduledTimer(withTimeInterval: 10, repeats: true) { _ in
+                    self.getData()
+                }
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         print("viewDidAppear")
-        getData()
-        Timer.scheduledTimer(withTimeInterval: 10, repeats: true) { _ in
-                    self.getData()
-                }
 //        view.backgroundColor = .systemBlue
     }
     
